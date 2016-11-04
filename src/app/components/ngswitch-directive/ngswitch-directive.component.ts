9bb79326c275e3d626ba12c0cb1dcb1cfdba1a79
Régis Oda <regis_oda@hotmail.com>
2016-11-04 09:59:32 -0200

    Diretive ngSwitch

--- src/app/components/ngswitch-directive/ngswitch-directive.component.ts
+++ src/app/components/ngswitch-directive/ngswitch-directive.component.ts
@@ -2,10 +2,13 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-ngswitch-directive',
-  templateUrl: './ngswitch-directive.component.html'
+  templateUrl: './ngswitch-directive.component.html',
+  styleUrls: ['./ngswitch-directive.component.css']
 })
 export class NgswitchDirectiveComponent implements OnInit {
 
+  tab : string = 'TAB1';
+  
   constructor() { }
 
   ngOnInit() {
